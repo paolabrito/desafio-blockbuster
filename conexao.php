20e2676f7331b77e5d7467925dee6e80ca3fb2ff
paolabrito <paolakta@hotmail.com>
2020-01-18 10:49:10 -0300

    alterando conexao

--- conexao.php
+++ conexao.php
@@ -3,20 +3,25 @@ $username = 'root';
 $password = 'almondega';
 try {
     $conn = new PDO('mysql:host=127.0.0.1;dbname=locadora;port=3306', $username, $password);
-    $categoria = $conn->prepare('SELECT * FROM aluguel');
+    $categoria = $conn->prepare('SELECT * FROM categoria');
     $categoria->execute();
 
 
     // categoria (categoria), titulo (filme), preço (filme), duração (filme), ano de lançamento (filme), classificação.
 
-
-    $filme = $conn->prepare('SELECT * FROM aluguel');
+    $filme = $conn->prepare('SELECT * FROM filme');
     $filme->execute();
 
-    $filme_categoria = $conn->prepare('SELECT * FROM aluguel');
+    $filme_categoria = $conn->prepare('SELECT * FROM filme_categoria');
     $filme_categoria->execute();
+
+    
 
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
+}
+
+while($row = $categoria->fetchAll(PDO::FETCH_ASSOC)) {
+    var_dump($row);
 }
 ?>
